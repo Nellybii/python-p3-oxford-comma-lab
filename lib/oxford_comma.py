254ae465e3da51aee0ed7b73d6cc3e2727916569
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-11-27 21:00:55 +0300

    oxford comma

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+my_list = oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+print(my_list)
+
